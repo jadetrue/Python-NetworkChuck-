b57927c98f2baffa376983e2da927a4df6c6ef95
Jade True <39219696+jadetruexo@users.noreply.github.com>
2022-03-05 17:03:09 +0000

    Learning the print function

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Helllo world")
+print('I am Iron Man')
+print("I am Tony Stark")
+print("No, I am Poppy")
+
+# multi-line string
+print("""Helllo world. 
+      I am Iron Man. 
+      I am Tony Stark.
+      No, I am Poppy""")
+
+# concatanating strings
+print("I am Iron Man. " + "No, I am Iron Man." + " No, I am Poppy.")
+
+print("I am Iron Man.\n" + "No, I am Iron Man.\n" + "No, I am Poppy.\n")
+
+# We are practicing Python.....IT'S AWESOME
+
+print("I am Poppy\n" * 100)
