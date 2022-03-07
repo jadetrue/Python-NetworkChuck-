3d0c40f2cf4051501e6698fbacb2636b19a4ec19
Jade True <39219696+jadetruexo@users.noreply.github.com>
2022-03-07 19:20:59 +0000

    New concept if-else statements

---
+++ if-else.py
@@ -0,0 +1,13 @@
+name = input("What is your name?\n")
+
+
+if name == "Ben": 
+  print("You're not welcome here.")
+else: 
+  print("Hello " + name + ", thank you so much for coming in today.")
+
+if 4 < 3:
+  print("Yup it's true")
+  print("It's true")
+else:
+  print("Nope not true")
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ print("Hello, weclome to NetworkChuck coffee!!!")
 
 name = input("What is your name?\n")
 
-print("Hello " + name + ", thank you so much for coming in today.")
-
+if name == "Ben": 
+  print("You're not welcome here.")
+  exit()
+else: 
+  print("Hello " + name + ", thank you so much for coming in today.")
+  
 menu = "Americano,\n Cappuccino,\n Latte,\n Mocha,\n Espresso\n"
 
 order = input("What would you like? Here is our menu: " + menu)
@@ -21,3 +25,4 @@ number_of_coffees = input("How many coffee's would you like?\n")
 total = price * int(number_of_coffees)
 
 print("Thank you, " + number_of_coffees + " " + order + "'s coming up for you." + " That'll be £" + str(total))
+
